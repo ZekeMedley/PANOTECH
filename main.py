57d22304733a44c42e19d6b8d1572688fa77998d
ZekeMedley <zekemedley@gmail.com>
2020-02-13 14:32:29 -0800

    Only write largest face.

--- main.py
+++ main.py
@@ -1,64 +1,19 @@
-# Main entry point for controlls.
-from pyserial import get_serial, send_command
 from webcam_face_extractor import make_webcam_face_getter
-from time import sleep
-
-def make_direction_decider(width):
-    DISTANCE_THRESHOLD = 0.1* width
-
-    def decider(faces):
-        try:
-            len(faces)
-        except:
-            return 's'
-        if len(faces) == 0:
-            return "s"
-
-        print("found " + str(len(faces)) + " faces")
-        # Find the face with the minimum distance from the center.
-        # f[0] is its x cord, f[2] is its width.
-#        target = max(faces, key=lambda f: abs(f[0] + f[2]/2 -
-#                                              width/2))
-
-        target = max(faces, key=lambda f: f[2] * f[3])
-        distance = target[0] + target[2] / 2 - width/2
-
-        if abs(distance) > DISTANCE_THRESHOLD:
-            if target[0] < width / 4:
-                return 'L'
-            elif target[0] <= width / 2:
-                return 'l'  #smaller
-            if target[0] > (3*width / 4):
-                return 'R'
-            elif target[0] >= width / 2:
-                return 'r'  # smaller
-            # Now trying a different mode of smaller movement
-        return "s"
-
-    return decider
 
 def main():
-    face_getter, width, height = make_webcam_face_getter()
-    controller = make_direction_decider(width)
-    ser = get_serial()
-
-    while 1:
-        faces = face_getter()
-        command = controller(faces)
-        print("[sending] " + command)
-        ser = send_command(ser, command)
-        # rest..
-        sleep(2)
-        if ( command == 'L' or command == 'R' ):
-            ser = send_command(ser, 'D')
-            # After this is sent, we need to give the
-            # machine some love and let it rest / do its work.
-            sleep(2)
-        elif ( command == 'l' or command == 'r' ):
-            ser = send_command(ser, 'd') # d1 is for a smaller movement
-            # After this is sent, we need to give the
-            # machine some love and let it rest / do its work.
-            sleep(2)
-
+    face_image_writer = make_webcam_face_getter()
+    
+    successive_failure_count = 0
+    failure_threshold = 10
+    
+    while True:
+        success = face_image_writer()
+        if not success:
+            successive_failure_count += 1
+            if successive_failure_count > failure_threshold:
+                print("[Fatal Error] too many errors in a row!")
+                # tail call main in desperate effort to maybe fix
+                main()
+        successive_failure_count = 0
+        
 main()
-
--- webcam_face_extractor.py
+++ webcam_face_extractor.py
@@ -10,55 +10,44 @@ def make_webcam_face_getter():
         camera = cv2.VideoCapture(1)
         cameraIndex = 1
 
-    print("camera: " + str(cameraIndex))
-    import time
-    time.sleep(10)
-
     # The classifier we use. HAAR is slower than some other options, but
     # is more accurate. We can tune this later.
-    faceCascade = cv2.CascadeClassifier(
-        "/home/pi/opencv/data/haarcascades/haarcascade_frontalface_default.xml")
-
-    # Get dimensions of camera.
-    width  = camera.get(3) # float
-    height = camera.get(4) # float
+    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
     def zoomer():
         nonlocal cameraIndex
         nonlocal imageCount
-        # Make a new camera every time so that there
-        # is no buffer that will mix us up.
-        camera = cv2.VideoCapture(cameraIndex)
+        nonlocal camera
 
         if not camera.isOpened():
+            camera.release()
             print("[error] couldn't open camera. Aborting and trying new index.")
             cameraIndex += 1
             cameraIndex = cameraIndex % 2
-            return []
+            camera = cv2.VideoCapture(cameraIndex)
+            return False
 
         success, img = camera.read()
 
         if not success:
             print("[error] Could't read from webcam.")
-            return
+            return False
+
         greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # Perform the detection with some standard params.
-        faces = faceCascade.detectMultiScale(
-            greyscale,
-            scaleFactor=1.3,
-            minNeighbors=3,
-            minSize=(30, 30)
-        )
+        faces = faceCascade.detectMultiScale(greyscale)
+        if len(faces) == 0:
+            return True
 
-        # Iterate over the found faces and write them away.
-        for (x, y, w, h) in faces:
-            colorFace = img[y:y + h, x:x + w]
-            # To write extracted face to file uncomment the line below
-            # this and create a directory called "example_faces" where the
-            # program will write the faces to.
-            cv2.imwrite("images/" + str(imageCount) + '.jpg', colorFace)
-            imageCount += 1
-        camera.release()
-        return faces
+        x, y, w, h = max(faces, key=lambda x: x[2] * x[3])
+        colorFace = img[y:y + h, x:x + w]
+        success = cv2.imwrite("images/" + str(imageCount) + '.jpg', colorFace)
 
-    return zoomer, width, height
+        if not success:
+             print("[error] Failed to write image to file.")
+             return False
+        imageCount += 1
+
+        return True
+
+    return zoomer
